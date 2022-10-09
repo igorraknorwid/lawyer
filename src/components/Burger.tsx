6abff1710365582e44c35a8a48343a844bb13358
Igor Rak <igorrakfed@gmail.com>
2022-10-09 11:08:16 +0200

    top menu repair 2

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -34,16 +34,19 @@ function  Menu (links:ILink[]){
 
 export default function TopNavbar(){
     const [state,setState] = React.useState(false); 
+    const [init,setInit] = React.useState(true); 
     return (
         <div >
             <div className = {`absolute z-20 w-[230px] h-[230px] right-0 top-10 bg-cyan-900 flex justify-center items-center ${state?"bg-cyan-400":"bg-[#0e1c39]"}`}>
                 <div className={state ? "burger-btn active" : "burger-btn"}
-                onClick={() => {setState(!state)}}>
+                onClick={() => {
+                    if(init){setInit(false)};
+                    setState(!state)}}>
                     <span></span> 
                 </div>
             </div>
             
-            <div className = {`absolute  top-10 z-10 xl:w-[500px] bg-[#0e1c39] pt-[230px] pb-10 px-10 hidden top_menu ${state?"active":"hide"} `}>              
+            <div className = {`absolute  top-10 z-10 xl:w-[500px] bg-[#0e1c39] pt-[230px] pb-10 px-10 ${init&&"hidden"}  top_menu ${state?"active":"hide"} `}>              
                 <ul className="text-white text-3xl">
                     <li className="mb-3">Witam</li>
                     <li className="mb-3">O nas</li>
