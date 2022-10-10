4ab935b14b66ca2a392f168f618509ff4ddc71fe
igorraknorwid <igorrakfed@gmail.com>
2022-10-10 11:29:35 +0200

    prime mobile

--- src/components/Awards.tsx
+++ src/components/Awards.tsx
@@ -1,34 +1,36 @@
 import React from "react";
-import {TAward} from "../types"
+import { TAward } from "../types";
 
-
-
-interface IAward{
-  images:TAward[]
+interface IAward {
+  images: TAward[];
 }
 
+export default function Awards({ images }: IAward) {
+  const [state, setState] = React.useState(false);
 
-export default function Awards ({images}:IAward) {
-    const [state, setState] = React.useState(false);
-   
-    const nextRef = React.useRef<HTMLDivElement>(null);
-    React.useEffect(() => {
-      const observer = new IntersectionObserver((entries) => {
-        const entry = entries[0];
-        if(entry.isIntersecting){setState(true)}
-        
-      });
-      if (nextRef.current) {
-        observer.observe(nextRef.current);
+  const nextRef = React.useRef<HTMLDivElement>(null);
+  React.useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        setState(true);
       }
-    }, []);
+    });
+    if (nextRef.current) {
+      observer.observe(nextRef.current);
+    }
+  }, []);
 
-   return(
-    <section className="py-20 px-20">
-         <div style={{height:"1px",backgroundColor:"red"}} ref={nextRef}></div>
-    <ul className="flex justify-between items-center">
-       {images.map(img=><li key={img.id}><img src={state?img.src:""} alt={img.alt}/></li>)}
-    </ul>
+  return (
+    <section className='py-20 px-20'>
+      <div style={{ height: "1px" }} ref={nextRef}></div>
+      <ul className='flex justify-between items-center'>
+        {images.map((img) => (
+          <li key={img.id}>
+            <img src={state ? img.src : ""} alt={img.alt} />
+          </li>
+        ))}
+      </ul>
     </section>
-   )
+  );
 }
--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -1,61 +1,66 @@
 import React from "react";
 import "../styles/burger.css";
 
-
-
-
-interface ILinkCildren{
-    id:number;
-    slug:string;
-    title:string;
+interface ILinkCildren {
+  id: number;
+  slug: string;
+  title: string;
 }
 
-interface ILink{
-    id:number;
-    slug:string;
-    title:string;
-    children?:ILinkCildren[]
-    open: false
+interface ILink {
+  id: number;
+  slug: string;
+  title: string;
+  children?: ILinkCildren[];
+  open: false;
 }
 
-
-
-
-
-function  Menu (links:ILink[]){
-
-    return(
-       <ul>
-        {links.map(link=><li key={link.id}><a href={link.slug}>{link.title}</a></li>)}
-       </ul>
-    )
- 
+function Menu(links: ILink[]) {
+  return (
+    <ul>
+      {links.map((link) => (
+        <li key={link.id}>
+          <a href={link.slug}>{link.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-
-
-
-export default function TopNavbar(){
-    const [state,setState] = React.useState(false); 
-    const [init,setInit] = React.useState(true); 
-    return (
-        <div >
-            <div className = {`absolute z-20 w-[230px] h-[230px] right-0 top-10 bg-cyan-900 flex justify-center items-center ${state?"bg-cyan-400":"bg-[#0e1c39]"}`}>
-                <div className={state ? "burger-btn active" : "burger-btn"}
-                onClick={() => {
-                    if(init){setInit(false)};
-                    setState(!state)}}>
-                    <span></span> 
-                </div>
-            </div>
-            
-            <div className = {`absolute  top-10 z-10 xl:w-[500px] bg-[#0e1c39] pt-[230px] pb-10 px-10 ${init&&"hidden"}  top_menu ${state?"active":"hide"} `}>              
-                <ul className="text-white text-3xl">
-                    <li className="mb-3">Witam</li>
-                    <li className="mb-3">O nas</li>
-                    <li className="">Kontakty</li>
-                </ul>
-            </div>
+export default function TopNavbar() {
+  const [state, setState] = React.useState(false);
+  const [init, setInit] = React.useState(true);
+  return (
+    <div className=''>
+      <div
+        className={`absolute xl:top-5 z-20 w-[90px] xl:w-[180px] h-[90px] xl:h-[180px] right-0 bg-cyan-900 flex justify-center items-center ${
+          state ? "bg-cyan-400" : "bg-[#0e1c39]"
+        }`}
+      >
+        <div
+          className={state ? "burger-btn active" : "burger-btn"}
+          onClick={() => {
+            if (init) {
+              setInit(false);
+            }
+            setState(!state);
+          }}
+        >
+          <span></span>
         </div>
-    )
+      </div>
+
+      <div
+        className={`absolute xl:top-5  z-10 w-[80%] xl:w-[500px] bg-[#0e1c39] pt-[90px]   xl:pt-[180px] pb-10 px-10 ${
+          init && "hidden"
+        }  top_menu ${state ? "active" : "hide"} `}
+      >
+        <ul className='text-white text-xl xl:text-3xl'>
+          <li className='mb-3 mt-3 '>Witam</li>
+          <li className='mb-3'>O nas</li>
+          <li className=''>Kontakty</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
