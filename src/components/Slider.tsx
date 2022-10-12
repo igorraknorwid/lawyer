3b68f08f38bef3bbc44ee5bda79b6cb260ff73de
igorraknorwid <igorrakfed@gmail.com>
2022-10-12 15:12:46 +0200

    slider 7

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -3,52 +3,51 @@ import Slider from "react-slick";
 import "../styles/slider.css";
 
 export default function SimpleSlider() {
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-  };
+  // const settings = {
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 2,
+  //   slidesToScroll: 1,
+  // };
   return (
-    <div>Hello</div>
-    // <Slider {...settings}>
-    //   <div className='py-5 px-20  '>
-    //     <img className='' src={"/stars.png"} alt={"starts"} />
-    //     <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-    //     <p className='text-center  text-xl'>
-    //       Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
-    //       Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
-    //       porad prawnych.
-    //     </p>
-    //     <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-    //     <p className='text-center'>Paulina K.</p>
-    //   </div>
-    //   <div className='py-5 px-20'>
-    //     <div className='flex justify-center items-center'>
-    //       <img className='' src={"/stars.png"} alt={"starts"} />
-    //     </div>
+    <Slider infinite={true} speed={500} slidesToShow={2} slidesToScroll={1}>
+      <div className='py-5 px-20  '>
+        <img className='' src={"/stars.png"} alt={"starts"} />
+        <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
+        <p className='text-center  text-xl'>
+          Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
+          Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
+          porad prawnych.
+        </p>
+        <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
+        <p className='text-center'>Paulina K.</p>
+      </div>
+      <div className='py-5 px-20'>
+        <div className='flex justify-center items-center'>
+          <img className='' src={"/stars.png"} alt={"starts"} />
+        </div>
 
-    //     <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-    //     <p className='text-center text-xl'>
-    //       Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
-    //       Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
-    //       porad prawnych.
-    //     </p>
-    //     <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-    //     <p className='text-center'>Paulina K.</p>
-    //   </div>
-    //   <div>
-    //     <h3>3</h3>
-    //   </div>
-    //   <div>
-    //     <h3>4</h3>
-    //   </div>
-    //   <div>
-    //     <h3>5</h3>
-    //   </div>
-    //   <div>
-    //     <h3>6</h3>
-    //   </div>
-    // </Slider>
+        <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
+        <p className='text-center text-xl'>
+          Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
+          Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
+          porad prawnych.
+        </p>
+        <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
+        <p className='text-center'>Paulina K.</p>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
   );
 }
