49b72806064c30e4992172e4daf02f0a28e567d5
igorraknorwid <igorrakfed@gmail.com>
2022-10-13 09:48:19 +0200

    slider swiper nav loop

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,66 +1,12 @@
 import React from "react";
-// import Slider from "react-slick";
-// import "../styles/slider.css";
-
-// const MySlider = () => {
-//   const customSlider = React.useRef<Slider>(null);
-//   const settings = {
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 2,
-//     slidesToScroll: 1,
-//   };
-//   const wishImage = (
-//     <Slider ref={customSlider} {...settings}>
-//       <div className='py-5 px-20  '>
-//         <img className='' src={"/stars.png"} alt={"starts"} />
-//         <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-//         <p className='text-center  text-xl'>
-//           Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
-//           Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
-//           porad prawnych.
-//         </p>
-//         <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-//         <p className='text-center'>Paulina K.</p>
-//       </div>
-//       <div className='py-5 px-20'>
-//         <div className='flex justify-center items-center'>
-//           <img className='' src={"/stars.png"} alt={"starts"} />
-//         </div>
-
-//         <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-//         <p className='text-center text-xl'>
-//           Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
-//           Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
-//           porad prawnych.
-//         </p>
-//         <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-//         <p className='text-center'>Paulina K.</p>
-//       </div>
-//       <div>
-//         <h3>3</h3>
-//       </div>
-//       <div>
-//         <h3>4</h3>
-//       </div>
-//       <div>
-//         <h3>5</h3>
-//       </div>
-//       <div>
-//         <h3>6</h3>
-//       </div>
-//     </Slider>
-//   );
-//   return <div>{/* {wishImage} */} hello</div>;
-// };
-
-// export default React.memo(MySlider);
-
-// Import Swiper React components
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Slide = () => {
   return (
@@ -81,10 +27,15 @@ const Slide = () => {
 export default () => {
   return (
     <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
       slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
+      navigation
+      loop={true}
+      // pagination={{ clickable: true }}
+      // scrollbar={{ draggable: true }}
+      // onSwiper={(swiper) => console.log(swiper)}
+      // onSlideChange={() => console.log("slide change")}
     >
       <SwiperSlide>
         <Slide />
