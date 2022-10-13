dd2b72b6fbfb063e34c2f47949f25fd94cf533d9
igorraknorwid <igorrakfed@gmail.com>
2022-10-13 08:05:51 +0200

    slider swiper

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,57 +1,80 @@
 import React from "react";
-import Slider from "react-slick";
-import "../styles/slider.css";
+// import Slider from "react-slick";
+// import "../styles/slider.css";
 
-const MySlider = () => {
-  const customSlider = React.useRef<Slider>(null);
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-  };
-  const wishImage = (
-    <Slider ref={customSlider} {...settings}>
-      <div className='py-5 px-20  '>
-        <img className='' src={"/stars.png"} alt={"starts"} />
-        <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-        <p className='text-center  text-xl'>
-          Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
-          Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
-          porad prawnych.
-        </p>
-        <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-        <p className='text-center'>Paulina K.</p>
-      </div>
-      <div className='py-5 px-20'>
-        <div className='flex justify-center items-center'>
-          <img className='' src={"/stars.png"} alt={"starts"} />
-        </div>
+// const MySlider = () => {
+//   const customSlider = React.useRef<Slider>(null);
+//   const settings = {
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//   };
+//   const wishImage = (
+//     <Slider ref={customSlider} {...settings}>
+//       <div className='py-5 px-20  '>
+//         <img className='' src={"/stars.png"} alt={"starts"} />
+//         <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
+//         <p className='text-center  text-xl'>
+//           Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
+//           Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
+//           porad prawnych.
+//         </p>
+//         <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
+//         <p className='text-center'>Paulina K.</p>
+//       </div>
+//       <div className='py-5 px-20'>
+//         <div className='flex justify-center items-center'>
+//           <img className='' src={"/stars.png"} alt={"starts"} />
+//         </div>
 
-        <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-        <p className='text-center text-xl'>
-          Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
-          Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
-          porad prawnych.
-        </p>
-        <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
-        <p className='text-center'>Paulina K.</p>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
+//         <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
+//         <p className='text-center text-xl'>
+//           Firma przeprowadziła mnie przez bardzo trudny okres w moim życiu. Pan
+//           Berenji towarzyszył mi na każdym kroku i udzielał mi niesamowitych
+//           porad prawnych.
+//         </p>
+//         <div class='w-10 h-1 bg-[#cdb171] my-10 mx-auto'></div>
+//         <p className='text-center'>Paulina K.</p>
+//       </div>
+//       <div>
+//         <h3>3</h3>
+//       </div>
+//       <div>
+//         <h3>4</h3>
+//       </div>
+//       <div>
+//         <h3>5</h3>
+//       </div>
+//       <div>
+//         <h3>6</h3>
+//       </div>
+//     </Slider>
+//   );
+//   return <div>{/* {wishImage} */} hello</div>;
+// };
+
+// export default React.memo(MySlider);
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
+export default () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
   );
-  return <div>{/* {wishImage} */} hello</div>;
 };
-
-export default React.memo(MySlider);
