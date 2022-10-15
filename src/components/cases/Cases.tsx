c25a1650b8a68b82078f3c5943f22022ef24bb9f
Igor Rak <igorrakfed@gmail.com>
2022-10-15 09:23:29 +0200

    cases grid

--- src/components/cases/Cases.tsx
+++ src/components/cases/Cases.tsx
@@ -1,24 +1,64 @@
 import { TCase } from "../../types";
 import React from "react";
+
 interface ICases {
   cases: TCase[];
 }
 
-function CasesModal({ title, text }: TCase) {
-  const [isModal, setModal] = React.useState(false);
-  const [state, setState] = React.useState({} as TCase);
+interface ICase {
+  c: TCase;
+  closeModal(): void;
+}
+
+function CasesModal({ c, closeModal }: ICase) {
   return (
-    <div className='absolute top-0 bottom-0 left-0 flex justify-center items-center '>
-      <div className='bg-blue-600 text-xl text-white '>
-        <button>Close</button>
-        <h3>{title}</h3>
-        <p>{text}</p>
-        <button>More</button>
+    <div className="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center bg-red-500 ">
+      <div className="bg-blue-600 text-xl text-white">
+        <button onClick={closeModal}>CLOSE</button>        
+        <h3>{c.title}</h3>
+        <p>{c.text}</p>
+        <a>More</a>
       </div>
     </div>
   );
 }
 
-export default function Cases({ cases }: ICases) {
-  return <div></div>;
-}
+const Cases = ({ cases }: ICases) => {
+  const [isModal, setModal] = React.useState(false);
+  const [state, setState] = React.useState<TCase>({
+    id: 100,
+    text: "text",
+    title: "title",
+  });
+  const closeModal = () => {
+    setModal(false);
+  };
+  const openModal = (c: TCase) => {
+    setModal(true);
+    setState(c);
+  };
+  console.log(cases);
+  return (
+    <div className="w-8/12 m-auto">
+      {isModal && <CasesModal c={state} closeModal={closeModal} />}
+      <ul className="grid grid-cols-4 gap-x-8 gap-y-12">
+        {cases.map((c) => (
+          <li key={c.id} className="bg-white text-black py-10">
+            <div className="flex flex-col justify-center items-center">
+              <h3 className="text-3xl">{c.title}</h3>
+              <button className="text-3xl"
+                onClick={() => {
+                  openModal(c);
+                }}
+              >
+                +
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Cases;
