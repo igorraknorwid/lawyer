80a3960c62e1caa92d038d9160d7f313e8a708b5
igorraknorwid <igorrakfed@gmail.com>
2022-10-20 10:04:14 +0200

    content rozwod 2

--- src/components/Lawyer.tsx
+++ src/components/Lawyer.tsx
@@ -41,15 +41,16 @@ export default function Lawyer({ image_src, image_alt }: ILawyer) {
           <div className='w-10 h-1 bg-[#cdb171] my-10'></div>
           <div className='text-2xl xl:max-w-3xl'>
             <p className='mb-5'>
-              W kancelarii nasi prawnicy zajmujący się rozwodami w Los Angeles
+              W kancelarii nasi prawnicy zajmujący się rozwodami w Gdańsku
               rozumieją, że spór prawny z rodziną jest prawdopodobnie
               najtrudniejszym wyzwaniem, z jakim kiedykolwiek będziesz musiał
               się zmierzyć. Emocje są wysokie, co może utrudniać podejmowanie
               świadomych i racjonalnych decyzji dotyczących przyszłości. Jeśli
-              decyzje te mają wpływ na dzieci, stawka może być jeszcze wyższa.
-              Zatrudnienie doświadczonego prawnika zajmującego się prawem
-              rodzinnym, który pomoże Ci przejść przez te trudne czasy, pomoże
-              Ci zapewnić najlepszy wynik dla Ciebie i Twoich bliskich.
+              decyzje te mają wpływ na dzieci, stawka może być jeszcze
+              wyższa.Rozwód jest okolicznością kończącą małżeństwo. Zatrudnienie
+              doświadczonego prawnika zajmującego się prawem rodzinnym, który
+              pomoże Ci przejść przez te trudne czasy, pomoże Ci zapewnić
+              najlepszy wynik dla Ciebie i Twoich bliskich.
             </p>
             <p className='mb-5'>
               W kancelarii mamy ponad dwie dekady doświadczenia w prowadzeniu
@@ -62,12 +63,13 @@ export default function Lawyer({ image_src, image_alt }: ILawyer) {
               naszych klientów.
             </p>
             <p>
-              Jeśli zmagasz się ze sporem dotyczącym prawa rodzinnego, ważne
-              jest, abyś zrozumiał wszystkie dostępne opcje prawne. Zadzwoń do
-              firmy, aby umówić się na bezpłatną ocenę sprawy. Wysłuchamy Twojej
-              historii i potrzeb, ustalimy najlepszą strategię prawną dla Twojej
-              konkretnej sytuacji i dołożymy wszelkich starań, aby odpowiedzieć
-              na wszelkie Twoje pytania.
+              Rozwód jest ciężkim przeżyciem dla wszystkich stron. Jeśli zmagasz
+              się ze sporem dotyczącym prawa rodzinnego, ważne jest, abyś
+              zrozumiał wszystkie dostępne opcje prawne. Zadzwoń do firmy, aby
+              umówić się na bezpłatną ocenę sprawy. Wysłuchamy Twojej historii i
+              potrzeb, ustalimy najlepszą strategię prawną dla Twojej konkretnej
+              sytuacji i dołożymy wszelkich starań, aby odpowiedzieć na wszelkie
+              Twoje pytania. Rozwód Gdańsk.
             </p>
           </div>
         </div>
