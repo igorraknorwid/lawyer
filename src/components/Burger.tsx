799e34d7431ac1700bfb9095abc17d16e3951972
Igor Rak <igorrakfed@gmail.com>
2022-10-22 10:25:32 +0200

    menu 2

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -66,7 +66,7 @@ function Menu({ links, url_cuted_param }: IMenu) {
                   } `}
                 ></div>
                 <div className="pl-10">
-                  {index === 0 ? <a href='/'>Witam</a> : <a href={`/${link.slug}`}>{link.title}</a>  }
+                  {index === 0 ? <a href='/'>Witam</a> : <a href={`/${link.slug}/`}>{link.title}</a>  }
                   {/* <a href={`/${link.slug}`}>{link.title}</a>  */}
                   {/* <a href="/o-nas/">{link.title}</a>
                   {/* <a href='https://radca-test.pl/o-nas'>{link.title}</a> */}
