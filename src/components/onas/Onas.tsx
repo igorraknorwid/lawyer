42c00cd1e6821fb53fcb67371d4c1235369888e0
igorraknorwid <igorrakfed@gmail.com>
2022-10-27 14:42:31 +0200

    404 2

--- src/components/onas/Onas.tsx
+++ src/components/onas/Onas.tsx
@@ -10,7 +10,6 @@ export default function Onas({ about }: IOnas) {
   return (
     <div className='py-24'>
       <div>O NAS</div>
-      {/* <div className='markdown'>{about.data.attributes.about}</div> */}
       <div className='markdown'>
         <ReactMarkdown children={about.data.attributes.about} />
       </div>
