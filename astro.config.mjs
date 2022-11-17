f9d36d15e3d8e9007f531a2a1d4a90c3fc6660e1
igorraknorwid <igorrakfed@gmail.com>
2022-11-17 10:22:45 +0100

    meta data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://radca.netlify.app/",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()]
 });
